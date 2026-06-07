d814373ce51fac6076c8574bdcb3a63843f9c434
cheikh <coolrass1@gmail.com>
2026-06-07 16:33:00 +0100

    Update Wil's admission year to 2008 and add 25 years overall legal experience

--- app/components/solicitors/Solicitor2.jsx
+++ app/components/solicitors/Solicitor2.jsx
@@ -32,10 +32,10 @@ const Solicitor2 = () => {
           </h1>
           <h3 className="font-extrabold mb-2">Director / Solicitor </h3>
           <p className="py-2 text-justify">
-            Wil is a Solicitor of the Supreme Court of England &amp; Wales (admitted 2005), now in his <strong>18th year of post-qualification experience (PQE)</strong>. He is a co-Director and co-founder of JDS Solicitors Ltd.
+            Wil is a Solicitor of the Supreme Court of England &amp; Wales (admitted 2008), now in his <strong>18th year of post-qualification (PQE) and 25 years overall legal experience.</strong> He is a co-Director and co-founder of JDS Solicitors Ltd.
           </p>
           <p className="py-2 text-justify">
-            He has extensive expertise in civil litigation, business law, commercial property, immigration law, written submissions, risk management, and regulatory compliance. Wil also plays a leading role in the firm's compliance and supervision functions.
+            He has extensive expertise in civil litigation, business law, commercial property, immigration law, written submissions, risk management, and regulatory compliance. Wil also plays a leading role in the firm&apos;s compliance and supervision functions.
           </p>
           
 
